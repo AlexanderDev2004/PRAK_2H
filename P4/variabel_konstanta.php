3ac98f8feaabf4dcbdca868f2f39ec3802791841
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-09-19 21:43:10 +0700

    variabel_konstanta

---
+++ P4/variabel_konstanta.php
@@ -0,0 +1,14 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil perjumlahan $angka1 dan $angka2 adalah $hasil";
+
+$benar = true;
+$salah = false;
+echo "variable benar $benar, vaeriable salah $salah";
+
+define("nama_situs", "website.com");
+define("tahun_pendiei", 2024);
+echo "selamat datang di" . nama_situs . " tahun pendie " . tahun_pendiei;
+?>
