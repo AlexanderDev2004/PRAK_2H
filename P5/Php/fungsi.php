36b150f430e872d54bcbad4a33cb8bb05a2d475e
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-09-28 14:53:58 +0700

    fungsi

---
+++ P5/Php/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+    function perkenalan () {
+        echo "Perkenalan<br>";
+        echo "Saya Alexander <br>";
+        echo "Saya sedang belajar PHP <br>";
+    } 
+
+    perkenalan();
+    perkenalan();
+?>
