23a43979fa6c9a8de63c3d71ced2a93086d682bd
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-10-01 08:06:43 +0700

    array_2

---
+++ P6/array_2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php  
+    $dosen = [
+        'nama' => 'elok nur hamdana',
+        'domisili' => 'Malang',
+        'jenis kelamin' => 'perempuan'
+    ];
+
+    echo "Nama : {$dosen['nama']}<br>";
+    echo "Domisili : {$dosen['domisili']}<br>";
+    echo "Jenis kelamin : {$dosen['jenis kelamin']}";
+?>    
+</body>
+</html>
