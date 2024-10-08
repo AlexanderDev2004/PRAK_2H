eb2d4cdfc1a0da3792ff540491b3c236ac51de6e
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-10-08 08:38:22 +0700

    empety

---
+++ P7/empty.php
@@ -0,0 +1,15 @@
+<?php
+//  $myarray  = array();
+//  if(empty($myarray)) {
+//      echo "Array kosong";
+//  } else {
+//      echo "Array ada isi";
+//  }
+
+
+if(empty($nonexistVar)) {
+    echo "Variable kosong";
+} else {
+    echo "Variable ada isi";
+}
+?>
